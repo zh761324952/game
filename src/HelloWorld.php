2ae84fcbdbe598121ace31327b1549e4ea3b2c01
zhanch <761324952@qq,com>
2021-02-10 22:16:58 +0800

    helloworld

---
+++ src/HelloWorld.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace zhanchmini\login;
+
+class HelloWorld
+{
+    /**
+     * @return string
+     */
+    public static function run()
+    {
+        return 'Hello World!';
+    }
+}
